e7dae8c9b164d7855d2345eb046fc114430d4b1d
lissms <felissms@gmail.com>
2020-10-13 11:00:27 +0200

    script

--- script.js
+++ script.js
@@ -3,31 +3,45 @@
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log("el numero aleatorio es", Math.ceil(Math.random() * 100));
 
 let randomNumber = getRandomNumber(100);
 const playButton = document.querySelector(".js-button");
 const message = document.querySelector(".js-clue");
 
-const number = document.querySelector(".js-number");
+const numberString = document.querySelector(".js-number");
 
+console.log("el numero aleatorio es", randomNumber);
 /* const number = parseInt(numberString.value); */
 /* no me fuenciona el parseInt, queria hacer una constante number string y luego pasarle a number con parse int, pero no me funciona */
 
 function getAMessage() {
-  if (number.value === randomNumber) {
+  let number = parseInt(numberString.value);
+  if (number === randomNumber) {
     message.innerHTML = "Has ganado campeon!!!";
-  } else if (number.value > randomNumber) {
+  } else if (number > randomNumber < 100) {
     message.innerHTML = "Demasiado alto.";
-  } else if (number.value < randomNumber) {
+  } else if (number < randomNumber) {
     message.innerHTML = "Demasiado bajo..";
-  } else if (number.value > 100) {
-    message.innerHTML = "El número debe estarentre 1 y 100.";
-  } else if (number.value < 1) {
-    message.innerHTML = "El número debe estarentre 1 y 100.";
-  } else if (number.value === "") {
-    message.innerHTML = "El número debe estarentre 1 y 100.";
+  } else if (number > 100) {
+    message.innerHTML = "El número debe estar entre 1 y 100.";
+  } else if (number < 1) {
+    message.innerHTML = "El número debe estar entre 1 y 100.";
+  } else if (number === NaN) {
+    message.innerHTML = "El número debe estar entre 1 y 100.";
   }
+  console.log("number.value", number);
 }
 
 playButton.addEventListener("click", getAMessage);
+
+/* funcion de intentos */
+const attemp = document.querySelector(".js-attemp");
+let initial = 0;
+let nextTry = 1;
+
+function getNumberAttemp() {
+  initial = initial + nextTry;
+  return (attemp.innerHTML = initial);
+}
+
+playButton.addEventListener("click", getNumberAttemp);
